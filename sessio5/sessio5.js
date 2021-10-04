10a5647ae2b066791d92364a42100762b93938af
josep <olmosescolatreball@gmail.com>
2021-10-04 17:46:31 +0200

    acabat sessio 5

--- sessio5/sessio5.js
+++ sessio5/sessio5.js
@@ -25,22 +25,66 @@ Validar si una persona era major edat, en la data donada.
 */
 
 function segonsCumple() {
-  let now = Date.now();
-  let birthday = new Date(2021,12,08);
-  console.log('ARA: ',now);
-  console.log('CUMPLE',birthday.getTime());
-  
-  let diff = birthday-now;
-  console.log('falten :',diff,' milisegons');
+    let now = Date.now();
+    let birthday = new Date(2021, 12, 08);
+    console.log("ARA: ", now);
+    console.log("CUMPLE", birthday.getTime());
+
+    let diff = birthday - now;
+    console.log("falten :", diff, " milisegons");
 }
 
 function demanadata() {
-    let date=new Date();
-    let introData=prompt("introdueix una data: (dd/mm/yyyy)",date.getTime(),date.getMonth(),date.getFullYear())
+    let date = new Date();
+    let introData = prompt(
+        "introdueix una data: (dd/mm/yyyy)",
+        date.getTime(),
+        date.getMonth(),
+        date.getFullYear()
+    );
     // fdate.getDate()+'/'+(date.getMonth()+1)+'/'+date.getYear());
 
-    
-    console.log('la data introduida es: ',date);
+    console.log("la data introduida es: ", date);
+}
 
-    
+/*
+Math
+Fes un programa que demani dos números a l’usuari i retorni un valor aleatori enter comprès entre aquests dos números, ambdós inclosos
+*/
+
+function mathAmbdosInclos(min, max) {
+    for (let index = 0; index < 100; index++) {
+        console.log(parseInt(Math.random() * (max + 1 - min) + min));
+    }
+}
+
+/*
+Number
+Fes un programa on es forci el resultat NaN i es mostri per pantalla.
+
+
+Fes un programa on es forci el resultat -Infinity o Infinity i es mostri per pantalla.
+
+Fes un programa per validar que no es un número
+
+Valida la longitud del campo código postal
+*/
+
+function retornaNAN() {
+    console.log(Math.sqrt(-1));
+}
+function retornaInfinit() {
+    console.log(Number.MAX_VALUE * -2);
+}
+
+function validarNaN() {
+  return console.log(parseInt(Math.sqrt(-1)));
+}
+
+function validarCodPostal(codPostal) {
+  console.log(parseInt(codPostal));
+    if(codPostal.length==5 && parseInt(codPostal)>0 && parseInt(codPostal)<57073){
+      return console.log('es un codi postal');
+    }
+    return console.log('no es un codi postal');
 }
