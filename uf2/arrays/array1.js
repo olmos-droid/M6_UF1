b8db7248c9a1de681a094fcd1edbc33e1e10b3c7
josep <olmosescolatreball@gmail.com>
2021-11-15 18:01:43 +0100

    15/11

--- uf2/arrays/array1.js
+++ uf2/arrays/array1.js
@@ -1,13 +1,44 @@
-var arraytest= [1,7,3,89,3,8,34,8,35,90876,-5];
-var arraytest2= ['pera','Mandarina','kiwi','banana', 'Plàtan','pera','Pera'];
+var arraytest = [1, 7, 3, 89, 3, 8, 34, 8, 35, 90876, -5];
+var arraytest2 = [
+    "pera",
+    "Mandarina",
+    "kiwi",
+    "banana",
+    "Plàtan",
+    "pera",
+    "Pera",
+];
 
-function sortArray(){
+function sortArray() {
     console.log("sensde ordenar: " + arraytest);
-    arraytest.sort();
-    console.log("ordenat:"+ arraytest );
+    arraytest.sort(function(a,b){return b-a});
+    console.log("ordenat:" + arraytest);
 }
-function sortArray2(){
+function sortArray2() {
     console.log("sensde ordenar: " + arraytest2);
-    arraytest2.sort((a,b)=> a.localeCompare(b));
-    console.log("ordenat:"+ arraytest2);
+    arraytest2.sort((a, b) => a.localeCompare(b));
+    console.log("ordenat:" + arraytest2);
 }
+
+// Amb el mètode “sort” i “slice” fes un programa que mostri els tres números més grans emmagatzemats a un array.
+function tres() {
+    arraytestSort = arraytest.sort();
+    console.log(arraytestSort);
+    console.log(arraytestSort.slice(-3));
+}
+
+// Amb el mètode “splice”, fes un programa en Javascript que tingui implementada la següent funció:
+// Entrada: dos arrays, un amb valors numèric i un amb valors alfabètics
+// Sortida: retorna un array amb els tres valors numèrics més grans i els tres elements alfabètics més baixos.
+// Exemple: // Array numèric: [1,5,8,999,43,-1]
+// Array alfabètic: [“Java”,”hola”,”zebra”,”il·lusió”,”Al·lèrgic”,”cap-i-cua”]
+// Sortida: [8,43,999,”Il·lusió”,”Java”,”zebra”]
+
+arraynum = [1, 534, 7, 8, 3, 34, 32];
+arraylletres = ["gdfegdf", "aaa", "bbb", "ftgkji", "zds", "ttttt"];
+
+arraynum.sort(function(a,b){
+    return b-a; 
+})
+
+arraaynum.splice();
---
+++ uf2/arrays/array3.js
@@ -0,0 +1,44 @@
+function parseando() {
+    
+    var matriu = JSON.parse(document.getElementById("matriu").value);
+    // console.table(matriu);
+    
+    let m1 = matriu[0];
+    let m2 = matriu[1];
+    // console.table(m1);
+    // console.table(m2);
+
+    var newmatriu=new Array(m1.length);
+
+    for (let x = 0; x < newmatriu.length; x++) {
+        newmatriu[x]= new Array(m1[x].length)
+        
+    }
+    console.table(newmatriu);
+
+    // console.table(m1);
+    // console.table(m2);
+
+    for (let x = 0; x < matriu.length; x++) {
+        for (let y = 0; y < matriu[x].length; y++) {
+            newmatriu[x][y]=0;
+            for (let z = 0; z < m2.length; z++) {
+                newmatriu[x][y] += m1[x][z] * m2[y][z];
+            }
+        }
+    }
+    console.table(newmatriu);
+}
+
+[
+    [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ],
+    [
+        [2, 2, 2],
+        [2, 2, 2],
+        [2, 2, 2],
+    ],
+];
