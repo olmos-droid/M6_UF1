79012784a2263eb5b1fe029142621fae5b415064
“Josep” <olmosescolatreball@gmail.com>
2021-11-29 15:39:20 +0100

    home to school

--- uf2/arrays/sudoku/sudoku.js
+++ uf2/arrays/sudoku/sudoku.js
@@ -1,23 +1,55 @@
 const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const MAT_INICIAL =
     "[[0,0,0,0,0,0,1,0,0],[0,8,1,0,9,3,0,4,5],[4,0,0,0,5,0,0,3,2],[9,0,0,5,0,4,0,7,8],[0,5,8,3,0,0,0,0,9],[0,0,0,8,2,0,0,0,0],[5,0,0,2,7,0,0,8,4],[6,0,4,1,3,8,2,0,7],[8,0,2,9,0,0,6,0,0]]";
-var matriu;
+var matriu = [[]];
 var posX;
 var posY;
 
 function start() {
-    let posiblidades = [];
-    draw();
+    let cont =0
+    document.getElementById("matriu").value = MAT_INICIAL;
+    matriu = JSON.parse(MAT_INICIAL);
+    console.table(matriu);
+    document.getElementById("sudoku").innerHTML =
+        "<table id='table' class=' table-bordered' > <tbody id='tbody'> </tbody></table>";
     for (let y = 0; y < matriu.length; y++) {
-        draw();
-        for (let x = 0; x < matriu.length; x++) {
-            if (matriu[y][x] == 0) {
-                console.log("y: " + y, "x: " + x);
-                posiblidades = checkNumberXY(x, y); // buscamos las posiblidades en esa posicion de la matriz
-                console.log(posiblidades);
+        document.getElementById("tbody").innerHTML +=
+            "<tr id='" + y + "'></tr>";
+        for (let x = 0; x < matriu[y].length; x++) {
+            if (matriu[y][x] != 0) {
+                document.getElementById(y).innerHTML +=
+                    "<td style='color:blue'> " + matriu[y][x] + " </td>";
+            } else {
+                matriu[y][x];
+                document.getElementById(y).innerHTML += "<td>_</td>";
             }
         }
     }
+    for (let x = 0; x < matriu.length; x++) {
+        for (let y = 0; y < matriu.length; y++) {
+            if (matriu[x][y]==0){
+               cont++;
+               
+            }
+
+        }
+    }
+}
+
+/*function start() {
+    let posiblidades = [];
+    draw();
+    for (let y = 0; y < matriu.length; y++) {
+        for (let x = 0; x < matriu.length; x++) {
+            if (matriu[y][x] == '0') {
+                console.log("pos yx -----------: ", y, x);
+                posiblidades = checkNumberXY(y, x); // buscamos las posiblidades en esa posicion de la matriz
+                console.log('posiblidades.length',posiblidades.length);
+                console.log('valor matriu : #',matriu[y][x])
+            }
+        }
+    }
+    console.log("movida");
 }
 function draw() {
     document.getElementById("matriu").value = MAT_INICIAL;
@@ -41,41 +73,22 @@ function draw() {
     console.table(matriu); //matriu inicial
 }
 
-function checkNumberXY() {
-    console.log(posX,posY);
-    let ejeX = []; // acomulacio de numeros que hi han en el eix x
-    let ejeY = []; // acomulacio de numeros  que hi han en eñ eix y
+function checkNumberXY(py, px) {
+    var numEjes = []; // acomulacio de numeros  que hi han en els eixos
     let filtroArray = new Set();
-    let numFilaCol = [];
 
     for (let y = 0; y < matriu.length; y++) {
-        //recorremos las posicion en el eje y (vertical)
-        ejeY.push(matriu[y][posX]); //guardamos los NUMBERS encontrados sin repetir en el set z
+
+        numEjes.push(matriu[y][px]); //guardamos los NUMBERS encontrados sin repetir en el set z
     }
     for (let x = 0; x < matriu.length; x++) {
-        //recorremos las posiciones en el eje x (horizontal)
-        ejeX.push(matriu[posY][x]); //guardamos los NUMBERS encontrados sin repetir en el set z
+
+        numEjes.push(matriu[py][x]); //guardamos los NUMBERS encontrados sin repetir en el set z
     }
 
-    numFilaCol = Array.from(new Set(ejeY.concat(ejeX)));
-
     filtroArray = NUMBERS.filter(function (num) {
-        return !numFilaCol.includes(num);
+        return !numEjes.includes(num);
     });
+    console.log(filtroArray, "este es el Set de numeros que podrian ir ");
     return filtroArray;
-}
-function findNumbers(ejeX, ejeY) {
-    filtroArray = new Set();
-    numFilaCol = Array.from(new Set(ejeY.concat(ejeX)));
-    filtroArray = NUMBERS.filter(function (num) {
-        !numFilaCol.includes(num);
-    });
-    return filtroArray;
-}
-
-function noIncludes(arr) {
-    filtroArray = NUMBERS.filter(function (num) {
-        return !arr.includes(num);
-    });
-    return filtroArray;
-}
+}*/
