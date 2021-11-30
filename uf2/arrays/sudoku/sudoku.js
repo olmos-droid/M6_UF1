f72f2fa358c26ab432228226221eb43bb8aaeaee
“Josep” <olmosescolatreball@gmail.com>
2021-11-30 15:41:13 +0100

    testing

--- uf2/arrays/sudoku/sudoku.js
+++ uf2/arrays/sudoku/sudoku.js
@@ -2,8 +2,6 @@ const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const MAT_INICIAL =
     "[[0,0,0,0,0,0,1,0,0],[0,8,1,0,9,3,0,4,5],[4,0,0,0,5,0,0,3,2],[9,0,0,5,0,4,0,7,8],[0,5,8,3,0,0,0,0,9],[0,0,0,8,2,0,0,0,0],[5,0,0,2,7,0,0,8,4],[6,0,4,1,3,8,2,0,7],[8,0,2,9,0,0,6,0,0]]";
 var matriu = [[]];
-var posX = 0;
-var posY = 0;
 
 function start() {
     document.getElementById("matriu").value = MAT_INICIAL;
@@ -11,9 +9,50 @@ function start() {
     console.table(matriu);
     draw();
 }
+function resol() {
+    var cont = 0;
+    var posX = 0;
+    var posY = 0;
+    var numEjes = new Set(); // acomulacio de numeros  que hi han en els eixos
+
+    do {
+        cont=0;
+        for (let x = 0; x < matriu.length; x++) {
+            for (let y = 0; y < matriu.length; y++) {
+                cont++
+                if (matriu[x][y] == 0) {
+                    for (let z = 0; z < matriu.length; z++) {
+                        if (matriu[x][z] != 0) {
+                            numEjes.add(matriu[x][z]);
+                        }
+                    }
+                    for (let z = 0; z < matriu.length; z++) {
+                        if (matriu[z][y] != 0) {
+                            numEjes.add(matriu[z][y]);
+                        }
+                    }
+                    filtroArray = NUMBERS.filter(function (num) {
+                        return !numEjes.has(num);
+                    });
+                    console.log("filtro ", filtroArray);
+
+                    if (filtroArray.length == 1) {
+                        matriu[x][y] = filtroArray[0];
+                        console.log("Trobat a la pos: y: " + y + " x: " + x);
+                        draw();
+                        cont = 0;
+                        numEjes = new Set(); // acomulacio de numeros  que hi han en els eixos
+                    } else {
+                        numEjes = new Set(); // acomulacio de numeros  que hi han en els eixos
+                    }
+                }
+            }
+        }
+    } while (cont < 82);
+}
 
 function oneShoot() {
-    var numEjes = []; // acomulacio de numeros  que hi han en els eixos
+    numEjes = []; // acomulacio de numeros  que hi han en els eixos
     let filtroArray = new Set();
 
     if (matriu[posY][posX] == 0) {
@@ -43,7 +82,6 @@ function oneShoot() {
             matriu[posY][posX] = filtroArray[0];
             console.table(matriu);
             draw();
-            
         }
         if (posX == 8) {
             posX = 0;
@@ -63,26 +101,22 @@ function oneShoot() {
     return filtroArray;
 }
 
-
-
-
 function draw() {
     document.getElementById("sudoku").innerHTML =
         "<br> <table id='table' class='' > <tbody id='tbody'> </tbody></table>";
-    for (let y = 0; y < matriu.length; y++) {
-        document.getElementById("tbody").innerHTML += "<tr id=" + y + "></tr>";
-        for (let x = 0; x < matriu[y].length; x++) {
-            if (matriu[y][x] != 0) {
-                document.getElementById(y).innerHTML +=
+    for (let x = 0; x < matriu.length; x++) {
+        document.getElementById("tbody").innerHTML += "<tr id=" + x + "></tr>";
+        for (let y = 0; y < matriu.length; y++) {
+            if (matriu[x][y] != 0) {
+                document.getElementById(x).innerHTML +=
                     "<td > <button class='btn btn-outline-dark' id='" +
-                    y +
                     x +
+                    y +
                     "' >" +
-                    matriu[y][x] +
+                    matriu[x][y] +
                     "</button></td>";
             } else {
-                matriu[y][x];
-                document.getElementById(y).innerHTML +=
+                document.getElementById(x).innerHTML +=
                     "<td > <button class='btn btn-outline-dark'  id='" +
                     y +
                     x +
@@ -91,60 +125,3 @@ function draw() {
         }
     }
 }
-
-/*function start() {
-    let posiblidades = [];
-    draw();
-    for (let y = 0; y < matriu.length; y++) {
-        for (let x = 0; x < matriu.length; x++) {
-            if (matriu[y][x] == '0') {
-                console.log("pos yx -----------: ", y, x);
-                posiblidades = checkNumberXY(y, x); // buscamos las posiblidades en esa posicion de la matriz
-                console.log('posiblidades.length',posiblidades.length);
-                console.log('valor matriu : #',matriu[y][x])
-            }
-        }
-    }
-    console.log("movida");
-}
-function draw() {
-    document.getElementById("matriu").value = MAT_INICIAL;
-    matriu = JSON.parse(MAT_INICIAL);
-    //peimera vegada que pinta
-    document.getElementById("sudoku").innerHTML =
-        "<table id='table' class=' table-bordered' > <tbody id='tbody'> </tbody></table>";
-    for (let y = 0; y < matriu.length; y++) {
-        document.getElementById("tbody").innerHTML +=
-            "<tr id='" + y + "'></tr>";
-        for (let x = 0; x < matriu[y].length; x++) {
-            if (matriu[y][x] != 0) {
-                document.getElementById(y).innerHTML +=
-                    "<td style='color:blue'> " + matriu[y][x] + " </td>";
-            } else {
-                matriu[y][x];
-                document.getElementById(y).innerHTML += "<td></td>";
-            }
-        }
-    }
-    console.table(matriu); //matriu inicial
-}
-
-function checkNumberXY(py, px) {
-    var numEjes = []; // acomulacio de numeros  que hi han en els eixos
-    let filtroArray = new Set();
-
-    for (let y = 0; y < matriu.length; y++) {
-
-        numEjes.push(matriu[y][px]); //guardamos los NUMBERS encontrados sin repetir en el set z
-    }
-    for (let x = 0; x < matriu.length; x++) {
-
-        numEjes.push(matriu[py][x]); //guardamos los NUMBERS encontrados sin repetir en el set z
-    }
-
-    filtroArray = NUMBERS.filter(function (num) {
-        return !numEjes.includes(num);
-    });
-    console.log(filtroArray, "este es el Set de numeros que podrian ir ");
-    return filtroArray;
-}*/
