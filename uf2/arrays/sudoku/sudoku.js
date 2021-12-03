94470c13062974155922edfaaaa6daa29b5a310d
“Josep” <olmosescolatreball@gmail.com>
2021-12-03 03:06:47 +0100

    terminado

--- uf2/arrays/sudoku/sudoku.js
+++ uf2/arrays/sudoku/sudoku.js
@@ -2,12 +2,12 @@ const NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 //"[[0,0,0,0,0,0,1,0,0],[0,8,1,0,9,3,0,4,5],[4,0,0,0,5,0,0,3,2],[9,0,0,5,0,4,0,7,8],[0,5,8,3,0,0,0,0,9],[0,0,0,8,2,0,0,0,0],[5,0,0,2,7,0,0,8,4],[6,0,4,1,3,8,2,0,7],[8,0,2,9,0,0,6,0,0]]";
 //"[[1,3,2,0,8,0,0,7,4],[0,0,8,7,3,0,2,0,0],[7,6,4,0,0,0,8,0,0],[4,2,0,5,0,0,0,1,9],[0,0,5,3,0,0,7,0,0],[6,0,0,1,0,0,0,5,2],[0,0,6,4,7,3,0,0,0],[0,4,9,0,0,1,0,0,0],[8,0,1,0,2,0,0,0,3]]";
 //"[[2,0,9,0,6,0,0,3,8],[0,0,4,5,0,8,1,0,0],[6,0,5,0,0,0,4,0,9],[0,5,0,3,4,0,0,2,0],[0,0,0,0,0,1,0,0,0],[4,7,0,0,0,6,8,9,1],[0,0,0,4,2,0,9,1,3],[0,4,2,9,1,0,6,0,0],[0,9,7,0,0,0,2,0,4]]";
-var matriu = [[]];
+var matriu =[[]]
+draw();
 
 function start() {
     let sudoku = document.getElementById("matriu").value;
     matriu = JSON.parse(sudoku);
-    console.table(matriu);
     draw();
 }
 function oneShoot() {
@@ -61,36 +61,35 @@ function oneShoot() {
 }
 function resol() {
     let end = false;
-    var delayInMilliseconds = 0; //1 second
     do {
         end = oneShoot();
     } while (end);
 }
 function draw() {
     document.getElementById("sudoku").innerHTML =
-        "<br> <table id='table' class='' > <tbody id='tbody'> </tbody></table>";
+        "<br> <table id='table' class='mx-auto w-auto' > <tbody id='tbody'> </tbody></table>";
     for (let x = 0; x < matriu.length; x++) {
         document.getElementById("tbody").innerHTML += "<tr id=" + x + "></tr>";
         for (let y = 0; y < matriu.length; y++) {
             if (matriu[x][y] != 0) {
                 document.getElementById(x).innerHTML +=
-                    "<td > <button class='btn btn-outline-secondary ' id='" +
+                    "<td > <button class='btn btn-outline-secondary custom ' id='" +
                     x +
                     y +
                     "' >" +
-                    matriu[x][y] + "&nbsp;" +
+                    matriu[x][y] +
                     "</button></td>";
             } else {
                 document.getElementById(x).innerHTML +=
-                    "<td > <button class='btn btn-outline-secondary'  id='" +
+                    "<td > <button class='btn btn-outline-secondary custom'  id='" +
                     x +
                     y +
                     "'> &nbsp;&nbsp;&nbsp; </button></td>";
             }
-            if (y == 2 || y == 5 || y == 8) {
+            if (y == 2 || y == 5) {
                 document.getElementById("" + x + "" + y + "").style.marginRight = "10px";
             }
-            if (x == 2 || x == 5 || x == 8) {
+            if (x == 2 || x == 5) {
                 document.getElementById("" + x + "" + y + "").style.marginBottom = "25px";
             }
         }
