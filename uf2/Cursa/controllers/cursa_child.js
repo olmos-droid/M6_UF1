8ba74dd3f6d45164c8e5d3dac5cff904dc59d416
“Josep” <olmosescolatreball@gmail.com>
2022-01-02 04:52:13 +0100

    hay control de edad , y sexo la cursa tiene corredores i la categoria tiene los corredores aptos

--- uf2/Cursa/controllers/cursa_child.js
+++ uf2/Cursa/controllers/cursa_child.js
@@ -1,37 +1,43 @@
 const metresTotals = opener.cursa._categoria._metres;
-const corredorActual = opener.cursa._categoria._inscrits[self.name];
+const corredorActual = opener.cursa._categoria._aptes[self.name];
 corredorActual._horaInici = opener.cursa._horaInici;
+const time_mms = 15;
+let metres;
+let myTime;
 
 function updateMetres() {
-  if (document.formChild.c_metres.value >= opener.cursa._categoria._metres) {
-    console.log(Date.now());
-  }
-  let time_mms = 15;
+ 
   myTime = setTimeout("showMetres()", time_mms);
 }
 function showMetres() {
-  const metres =
-    corredorActual._rendiment * ((Date.now() - opener.cursa._horaInici)/1000);
-  document.formChild.c_metres.value = Number.parseFloat(metres).toPrecision(5);
-  checkPoint(metres, opener.cursa._categoria._metres);
+  if (corredorActual._isRunning) {
+    checkPoint(metres, opener.cursa._categoria._metres);
+    metres =
+      corredorActual._rendiment *
+      ((Date.now() - opener.cursa._horaInici) / 1000);
+    document.formChild.c_metres.value =
+      Number.parseFloat(metres).toPrecision(5);
 
-  if (document.formChild.c_metres.value < metresTotals) {
-    updateMetres();
+    if (document.formChild.c_metres.value < metresTotals) {
+      updateMetres();
+    }
   }
 }
 
 document.formChild.c_name.value =
   corredorActual._nom + "  " + corredorActual._cognom;
-document.formChild.c_document.value = corredorActual._dni;
-let rend = corredorActual._rendiment;
-document.formChild.c_rendiment.value = Number.parseFloat(rend).toPrecision(2);
+document.formChild.c_document.value = corredorActual._document;
+document.formChild.c_rendiment.value = Number.parseFloat(
+  corredorActual._rendiment
+).toPrecision(2);
 document.formChild.c_horaInici.value = new Date(corredorActual._horaInici);
 
 function checkPoint(metresActual, metresTotalCursa) {
   if (metresActual >= metresTotalCursa) {
     corredorActual._horaFinal = Date.now();
     document.formChild.c_horaFinal.value = new Date(corredorActual._horaFinal);
-    corredorActual._isRunning = true;
+    corredorActual._isRunning = false;
+    clearTimeout(myTime);
     // alert(corredorActual._nom + " ja ha aribat");
   }
 }
